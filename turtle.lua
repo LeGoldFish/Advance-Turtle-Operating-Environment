1d6a3e8db43d8c4a29c65c0db53cf9cb9f00201c
KingofGamesYami <dirthsj@mcsdonline.org>
2014-05-16 19:39:34 -0500

    Create turtle.lua
    forward and back added in initial commit

---
+++ turtle.lua
@@ -0,0 +1,33 @@
+local oldturtle = {}
+
+local pos = {x = 0, y = 0, z = 0, f = 0}
+
+for k, v in pairs( turtle )
+  oldturtle[k] = turtle[k]
+end
+
+function turtle.forward()
+  if oldturtle.forward() then
+    if pos.f == 0 then pos.x = pos.x + 1
+    elseif pos.f == 1 then pos.y = pos.y + 1
+    elseif pos.f == 2 then pos.x = pos.x - 1
+    elseif pos.f == 3 then pos.y = pos.y - 1
+    end
+    return true
+  else
+    return false
+  end
+end
+
+function turtle.back()
+  if oldturtle.back() then
+    if pos.f == 0 then pos.x = pos.x - 1
+    elseif pos.f == 1 then pos.y = pos.y - 1
+    elseif pos.f == 2 then pos.x = pos.x + 1
+    elseif pos.f == 3 then pos.y = pos.y + 1
+    end
+    return true
+  else
+   return false
+  end
+end
