80c0be5525da6b990399133cb8e518d0f0016dfc
KingofGamesYami <dirthsj@mcsdonline.org>
2014-05-17 11:13:28 -0500

    Update slot.lua

--- apis/slot.lua
+++ apis/slot.lua
@@ -1,2 +1,14 @@
 local slots = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
 
+fSlot = {
+  isKnown = function(self)
+    if self.info then
+      return true
+    else
+      return false
+    end
+  end,
+  getInfo = function(self) 
+    return self.info 
+  end
+}
